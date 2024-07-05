67558af919f86286ad152d2760417435a102e0a7
anas gharbi <anasgh31@gmail.com>
2024-07-05 19:19:49 +0200

    1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mde_prototype_ionic',
+  webDir: 'www'
+};
+
+export default config;
